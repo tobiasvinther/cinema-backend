5fbf034a326405cbf8499b46ec6abc6fecf51bd7
tobiasvinther <tobi626i@stud.kea.dk>
2022-05-02 10:39:48 +0200

    Renamed some stuff

--- src/main/java/kea/cinema/CinemaApplication.java
+++ src/main/java/kea/cinema/CinemaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExamProjectApplication {
+public class CinemaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ExamProjectApplication.class, args);
+        SpringApplication.run(CinemaApplication.class, args);
     }
 
 }

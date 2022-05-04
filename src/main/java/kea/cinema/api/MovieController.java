a9e1310b54dd1093bbea5eb73711de0d6a7fe8cb
tobiasvinther <tobi626i@stud.kea.dk>
2022-05-04 20:17:53 +0200

    Hopefully fixed failing test.

--- src/main/java/kea/cinema/api/MovieController.java
+++ src/main/java/kea/cinema/api/MovieController.java
@@ -1,22 +1,34 @@
 package kea.cinema.api;
 
+import kea.cinema.entities.Movie;
+import kea.cinema.repositories.MovieRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @CrossOrigin
+//@CrossOrigin(origins = "http://127.0.0.1:5500/")
 @RestController
 @RequestMapping("api/movies") //change this to something relevant
 public class MovieController {
 
-    /*
-    private MovieService movieService;
-    private ShowingService showingService;
+    //private MovieService movieService;
+    //private ShowingService showingService;
+    MovieRepository movieRepository;
+
+    public MovieController(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
 
     //get all movies
     @GetMapping()
     public List<Movie> getMovies() {
-        return movieService.getMovies();
+        List<Movie> movieList = movieRepository.findAll();
+        System.out.println("Liste: " + movieList);
+        return movieList;
     }
 
+    /*
     //get a specific movie by id
     @GetMapping("/{id}")
     public Movie getMovie(@PathVariable int id) {
--- src/test/java/kea/cinema/api/MovieControllerTest.java
+++ src/test/java/kea/cinema/api/MovieControllerTest.java
@@ -2,7 +2,10 @@ package kea.cinema.api;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class MovieControllerTest {
 
     @BeforeEach

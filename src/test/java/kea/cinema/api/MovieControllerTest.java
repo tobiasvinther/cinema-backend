4ce5fe7ea8e9605a323ed1047a8f6370a7e16dcf
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2022-05-09 11:18:34 +0200

    Outcommented

--- src/test/java/kea/cinema/api/MovieControllerTest.java
+++ src/test/java/kea/cinema/api/MovieControllerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MovieControllerTest {
 
     @BeforeEach
